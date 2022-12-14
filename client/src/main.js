089d3b5c27891ab1e741f4236cd3cd87e96f34fb
Daniel Kahoun <113265820+danielkahoun@users.noreply.github.com>
2022-12-14 23:40:44 +0100

    feat(client): import Bootstrap & VueCookies

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,15 @@
 import { createApp } from 'vue'
+import VueCookies from 'vue-cookies'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+import 'bootstrap'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueCookies, { expires: '7d'})
 app.use(router)
 
 app.mount('#app')

cce5aad118f82c025d3b1725c69c5af1233ecb10
Daniel Kahoun <113265820+danielkahoun@users.noreply.github.com>
2022-12-14 23:39:46 +0100

    feat(routes): add dashboard routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +6,28 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/LoginView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/prehled',
+      name: 'prehled',
+      component: () => import('../views/dashboard/HomeView.vue')
+    },
+    {
+      path: '/rozvrh',
+      name: 'rozvrh',
+      component: () => import('../views/dashboard/ScheduleView.vue')
+    },
+    {
+      path: '/absence',
+      name: 'absence',
+      component: () => import('../views/dashboard/AbsenceView.vue')
+    },
+    {
+      path: '/uzivatele',
+      name: 'uzivatele',
+      component: () => import('../views/dashboard/UsersView.vue')
+    },
   ]
 })
 

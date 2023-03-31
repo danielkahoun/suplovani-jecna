89ea5d5ae1425ac2ac362475e65b4da1fb4485e9
Daniel Kahoun <113265820+danielkahoun@users.noreply.github.com>
2023-03-31 15:03:23 +0200

    update(npm): remove unused dependency

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import VueCookies from 'vue-cookies'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -8,7 +7,6 @@ import 'bootstrap'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(VueCookies, { expires: '7d'})
 app.use(router)
 
